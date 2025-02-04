ee8cd03b57408488b7bfdac48a235b325a50798c
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-02-04 21:51:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
     # Display the uploaded image
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
 
     # Preprocess the image
     preprocessed_image = preprocess_image(image)
